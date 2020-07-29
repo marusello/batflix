3fe46d157e2b5c72f5b67abea8e38ffab82a84a9
Marcelo Augusto <marceloaugusto11@hotmail.com.br>
2020-07-28 23:39:18 -0300

    setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Olivia Pope (Kerry Washington) passa seu tempo protegendo a reputação das personalidades estadunidenses da elite, evitando que nasçam grandes escândalos. A moça decide abrir uma empresa após deixar seu trabalho na Casa Branca, mas não consegue se desvencilhar totalmente de seu passado profissional. Sua equipe é formada por Harrison Wright (Columbus Short), Quinn Perkins (Katie Lowes), Stephen Finch (Henry Ian Cusick), Abby Whelan (Darby Stanchfield) e o hacker Huck Finn (Guillermo Diaz)."}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />         
+
+      <Footer />
     </div>
   );
 }
